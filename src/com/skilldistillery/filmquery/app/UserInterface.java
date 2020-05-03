ac13f86e3b0d93a39d0513c921e212493812780a
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-05-03 15:42:58 -0600

    Starting user story 2

--- src/com/skilldistillery/filmquery/app/UserInterface.java
+++ src/com/skilldistillery/filmquery/app/UserInterface.java
@@ -1,6 +1,9 @@
 package com.skilldistillery.filmquery.app;
 
+import java.util.Scanner;
+
 public class UserInterface {
+	public Scanner kb = new Scanner(System.in);
 	
 	public UserInterface() {
 		printBanner();
@@ -19,5 +22,20 @@ public class UserInterface {
 		System.out.println("1. Look up film by ID");
 		System.out.println("2. Look up film by keyword");
 		System.out.println("3. Exit");
+		System.out.print("> ");
+	}
+	
+	public void userInput() {
+		int menuChoice = kb.nextInt();
+		
+		switch(menuChoice) {
+		case 1:
+			
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		}
 	}
 }
