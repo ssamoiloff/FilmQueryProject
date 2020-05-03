b333774307d3acb19c7b3455cfb07f1fd543ba1d
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-05-03 16:56:24 -0600

    User story 2 complete

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -1,43 +1,67 @@
 package com.skilldistillery.filmquery.app;
 
-import java.util.List;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import com.skilldistillery.filmquery.database.DatabaseAccessor;
 import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
-import com.skilldistillery.filmquery.entities.Actor;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
-
 	DatabaseAccessor db = new DatabaseAccessorObject();
+	static Scanner input = new Scanner(System.in);
+	UserInterface ui;
 
 	public static void main(String[] args) {
 		FilmQueryApp app = new FilmQueryApp();
-//		app.test();
 		app.launch();
-	}
-
-//	private void test() {
-//    Actor actor = null;
-//		try {
-//		actor = db.findActorById(33);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//    System.out.println(actor);
-//	}
-
-	private void launch() {
-		Scanner input = new Scanner(System.in);
-
-		startUserInterface(input);
-
 		input.close();
 	}
 
-	private void startUserInterface(Scanner input) {
-		UserInterface ui = new UserInterface();
-	}
+	private void launch() {
+		ui = new UserInterface();
+		menuLogic();
 
+	}
+	
+	public void menuLogic() {
+		int option = 0;
+		do {
+			ui.printMenu();
+			option = input.nextInt();
+			switch(option) {
+			case 1:
+				option1();
+				break;
+			case 2:
+				option2();
+				break;
+			case 3:
+				System.out.println("\t\tGoodbye!");
+				break;
+			default:
+				System.out.println("\nPlease enter a valid menu option!\n");
+			}
+		} while(option != 3);
+	}
+	
+	public void option1() {
+		Film film = null;
+		System.out.print("\nEnter film ID: ");
+		try {
+			film = db.findFilmById(FilmQueryApp.input.nextInt());
+			if (film == null) {
+				System.out.println("Film not found!");
+			} else {
+				System.out.println();
+				ui.printFilmInfo(film);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void option2() {
+		
+	}
 }
--- src/com/skilldistillery/filmquery/app/UserInterface.java
+++ src/com/skilldistillery/filmquery/app/UserInterface.java
@@ -2,12 +2,13 @@ package com.skilldistillery.filmquery.app;
 
 import java.util.Scanner;
 
+import com.skilldistillery.filmquery.entities.Film;
+
 public class UserInterface {
 	public Scanner kb = new Scanner(System.in);
 	
 	public UserInterface() {
 		printBanner();
-		printMenu();
 	}
 	
 	public void printBanner() {
@@ -18,24 +19,19 @@ public class UserInterface {
 	}
 	
 	public void printMenu() {
-		System.out.println("Please make a selection from the menu:\n");
+		System.out.println("Please make a selection from the menu:");
 		System.out.println("1. Look up film by ID");
 		System.out.println("2. Look up film by keyword");
 		System.out.println("3. Exit");
 		System.out.print("> ");
 	}
 	
-	public void userInput() {
-		int menuChoice = kb.nextInt();
-		
-		switch(menuChoice) {
-		case 1:
-			
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		}
+	public void printFilmInfo(Film film) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Title:\t\t" + film.getTitle() + "\n");
+		sb.append("Year:\t\t" + film.getReleaseYear() + "\n");
+		sb.append("Rating:\t\t" + film.getRating() + "\n");
+		sb.append("Description:\t" + film.getDescription() + "\n");
+		System.out.println(sb);
 	}
 }
